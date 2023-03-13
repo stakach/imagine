8e3bcb775281b9de32ed18163a17462d8ae564d4
Stephen von Takach <steve@place.technology>
2023-03-13 17:55:32 +1100

    fix: skip video frames while detections are taking place
    also improve FPS tracking

--- src/imagine/detector.cr
+++ src/imagine/detector.cr
@@ -10,10 +10,13 @@ class Imagine::Detector
   # State of detector, stream might not always be available
   @state_mutex : Mutex = Mutex.new
   @processing_count : Int32 = 0
+  @performing_detection : Bool = false
   getter? processing : Bool = false
   getter? error : Bool = false
   getter last_error : Exception? = nil
+
   getter fps : FPS = FPS.new
+  @next_fps_window : FPS = FPS.new
 
   # video and NN model processing
   getter input : URI | Path
@@ -24,6 +27,7 @@ class Imagine::Detector
 
   def detections
     @fps = FPS.new
+    @next_fps_window = FPS.new
 
     invocation = 0
     @state_mutex.synchronize do
@@ -37,19 +41,32 @@ class Imagine::Detector
     nn_model = @model
     input_width, input_height = nn_model.input_resolution
     spawn { process_video(invocation, input_width, input_height) }
+    spawn { track_fps(invocation) }
 
     # yield any detections
     channel = Channel(StumpyCore::Canvas).new(1)
     while @processing && invocation == @processing_count
       @channel = channel
       canvas = channel.receive
-      @fps.reset if @fps.operations.zero?
+
+      if @fps.operations.zero?
+        @fps.reset
+        @next_fps_window.reset
+      end
+
+      @state_mutex.synchronize { @performing_detection = true }
       detections = nn_model.process(canvas)
+      @state_mutex.synchronize { @performing_detection = false }
+
       @fps.increment
-      yield(canvas, detections) unless detections.empty?
+      @next_fps_window.increment
+      yield(canvas, detections, @fps) unless detections.empty?
     end
   ensure
-    @state_mutex.synchronize { @processing = false }
+    @state_mutex.synchronize do
+      @processing = false
+      @performing_detection = false
+    end
   end
 
   def stop
@@ -67,6 +84,7 @@ class Imagine::Detector
     video.each_frame(input_width, input_height) do |canvas, _key_frame|
       @error = false
       @frame = canvas
+      next if @performing_detection # skip frames as the NN is slow
       break unless @processing && invocation == @processing_count
 
       # pass the latest frame to the neural net
@@ -82,5 +100,14 @@ class Imagine::Detector
     @error = true
     sleep 2
     spawn { process_video(invocation, input_width, input_height) }
+  end
+
+  protected def track_fps(invocation)
+    loop do
+      sleep 5
+      break unless @processing && invocation == @processing_count
+      @fps = @next_fps_window
+      @next_fps_window = FPS.new
+    end
   end
 end
